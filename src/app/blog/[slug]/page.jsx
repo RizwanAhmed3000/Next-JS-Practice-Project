e1ed7fa888ad674d338e4aad5543c61250f4b23d
RizwanAhmed3000 <rizz112khan@gmail.com>
2024-06-14 11:53:22 +0500

    single post slug page && post card

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,8 +1,25 @@
+import PostUser from '@/components/postUser/PostUser'
+import Image from 'next/image'
 import React from 'react'
 
 const SingleBlog = () => {
     return (
-        <div>SingleBlog page</div>
+        <div className='px-[100px] flex gap-14'>
+            <div className="left relative w-[300px] h-[500px] bg-red-300">
+                <Image src={"/post.jpg"} fill className='object-cover' />
+            </div>
+            <div className="right flex flex-col gap-8">
+                <div className="title">
+                    <h1 className='text-[38px] font-bold'>Title</h1>
+                </div>
+                <div className="user">
+                    <PostUser />
+                </div>
+                <div className="desc">
+                    Description
+                </div>
+            </div>
+        </div>
     )
 }
 
--- src/components/postCard/PostCard.jsx
+++ src/components/postCard/PostCard.jsx
@@ -3,12 +3,12 @@ import Link from "next/link"
 
 const PostCard = ({ post }) => {
     return (
-        <div className="flex flex-col gap-[20px] mb-[10px]">
-            <div className="flex">
+        <div className="flex flex-col gap-[20px] mb-[10px] ">
+            <div className="flex ">
                 {/* {post.img && <div className="w-[90%] h-[400px] relative">
                     <Image src={post.img} alt="" fill className="object-cover" />
                 </div>} */}
-                <div className="w-[100%] h-[400px] relative">
+                <div className="w-[90%] h-[400px] relative">
                     <Image src="/post.jpg" alt="" fill className="object-cover" />
                 </div>
                 <span className="text-[12px] rotate-[270deg] m-auto">{`post.createdAt?.toString().slice(4, 16)`}</span>
@@ -16,7 +16,7 @@ const PostCard = ({ post }) => {
             <div className="">
                 <h1 className="w-[90%] mb-[10px] text-[20px]">{`post.title`}</h1>
                 <p className="w-[90%] mb-[10px] font-[300] text-gray-500">{`post.body`}</p>
-                <Link className="underline" href={`/blog/${`post.slug`}`}>READ MORE</Link>
+                <Link className="underline" href={`/blog/post`}>READ MORE</Link>
             </div>
         </div>
     )
